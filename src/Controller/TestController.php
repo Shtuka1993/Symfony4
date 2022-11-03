66f866f5e400ab736623ebf3cd8854b11ef86533
Yuriy Shtokaylo <Shtokaylo.Yuriy@gmail.com>
2022-11-03 20:47:33 +0200

    Change route to use annotation

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,8 +2,14 @@
     namespace App\Controller;
     
     use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+
     class TestController
     {
+        /**
+         * @Route("/test/testing")
+         */
+
         public function action() {
            $number = random_int(0, 100);
 
