13b3aedfee97b7a474c9e20d8741590554f5fc39
Yuriy Shtokaylo <Shtokaylo.Yuriy@gmail.com>
2022-11-04 00:18:02 +0200

    Basic example o fabstractController helper methods

---
+++ src/Controller/ExampleController.php
@@ -0,0 +1,42 @@
+<?php
+    namespace App\Controller;
+
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+
+    class ExampleController extends AbstractController
+    {
+        /**
+         * @Route("lucky/number/{max}", name="app_lucky_number")
+         */
+        public function number(int $max):Response {
+            $number = random_int(0, $max);
+
+            return $this->render('/lucky/number.html.twig', ['number' => $number]);
+        }
+
+        /**
+         * @Route("generate", name="generate")
+         */
+        public function generate() {
+
+            return new Response($this->generateUrl('app_lucky_number', ['max' => 7]));
+        }
+
+        /**
+         * @Route("redirectG", name="redirect-generate")
+         */
+        public function redirectG() {
+
+            return $this->redirectToRoute("generate");
+        }
+
+        /**
+         * @Route("symfony", name="symfony")
+         */
+        public function symfony() {
+
+            return $this->redirect('http://symfony.com/doc');
+        }
+    }
