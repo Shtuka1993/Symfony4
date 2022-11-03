221378da708ce95da01e6c2dac6fcac471c64af2
Yuriy Shtokaylo <Shtokaylo.Yuriy@gmail.com>
2022-11-03 21:42:45 +0200

    Try to use render

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -1,18 +1,18 @@
 <?php
     namespace App\Controller;
     
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
 
-    class TestController
-    {
+    class TestController extends AbstractController
+    {   
         /**
          * @Route("/test/testing")
          */
-
         public function action() {
            $number = random_int(0, 100);
 
-           return new Response('<html><body>Number: ' . $number . '</body></html>');
+           return  $this->render('test/test.html.twig', ['number' => $number,]);
         }
     }
