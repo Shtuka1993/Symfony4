c45a0eb9f9bbbab553e31829c89c3fcedc32e235
Yuriy Shtokaylo <Shtokaylo.Yuriy@gmail.com>
2022-11-06 20:11:24 +0200

    Add example for json response and get config

--- src/Controller/RequestResponseController.php
+++ src/Controller/RequestResponseController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class RequestResponseController extends AbstractController
 {
@@ -44,5 +45,22 @@ class RequestResponseController extends AbstractController
         $response->headers->set('Content-Type', 'text/css');
 
         return $response;
+    }
+
+    /**
+     * @Route("configExample", name="configExample")
+     */
+    public function config() {
+        $contentsDir = $this->getParameter('kernel.project_dir').'/contents';
+
+        return new Response($contentsDir);
+    }
+
+    /**
+     * @Route("JSONexample", name="JSONexample")
+     */
+    public function jsonExample(): JsonResponse {
+
+        return $this->json(['username' => 'jane.doe']);
     }
 }
